cd03583c8bd5bcb837fb68341c431027b338a019
Martin <m>
2021-06-24 17:58:21 -0300

    Agrego ejercicio 3

---
+++ ej3/ProcesoA.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <dirent.h>
+
+#define err(msg, arg){ fprintf(stderr, "%s, vea %s -h\n", msg, arg); exit(1);}
+
+#define help(arg) { fprintf(stdout, "Ejemplo de ejecucion:\n ./path-del-ejecutable path-del-fifo carpeta-de-facturacion/\nEjemplo: %s pipe/fifo facturacion/\n", arg); exit(1);}
+float facturacion(const char*, const char*);
+float facturacion_mensual(const char*);
+float facturacion_anual_o_media(const char*, int);
+
+void main(int argc, char*argv[]){
+
+	if(argc > 1 && strcmp(argv[1], "-h") == 0){
+		help(argv[0]);
+	}
+
+	if(argc < 3)
+		err("Argumentos insuficientes", argv[0]);
+	
+	char *instruccion = (char*)malloc(sizeof(char)*50);
+
+	int fd = open(argv[1], O_RDONLY);
+	read(fd, instruccion, sizeof(instruccion)*50);
+	close(fd);
+
+	const char opc = strtok(instruccion, "-")[0];
+	
+	if(opc - '0' == 4)
+		return;
+	
+	const char* anio = strtok(NULL, "-");
+	const char* mes = strtok(NULL, "-");
+
+	char*folder_path = (char*)malloc(sizeof(char)*(strlen(argv[2]) + strlen(anio) + strlen(mes)));
+
+	folder_path = argv[2];
+	strcat(folder_path, anio);
+
+	float res;
+	if(opc - '0' == 1)
+		res = facturacion_mensual(strcat(folder_path, mes));
+	else
+		res = facturacion_anual_o_media(folder_path, opc - '0');
+
+	fd = open(argv[1], O_WRONLY);
+	write(fd, &res, sizeof(res));
+	close(fd);
+	return;
+}
+
+float facturacion_mensual(const char* path_name){
+	
+	float total = 0, value;
+	FILE*arch = fopen(path_name, "rt");
+
+	if(!arch){
+		return 0;
+	}
+	
+	while(fscanf(arch, "%f\n", &value) != EOF){
+		total += value;
+	}
+	return total;
+}
+
+float facturacion_anual_o_media(const char* folder_path, int opc){
+
+	float total = 0;	
+	int count = 0;
+	
+	DIR *d;
+	struct dirent *folder;
+	d = opendir(folder_path);
+
+	if(d){
+		while((folder = readdir(d)) != NULL){
+			if( folder->d_type == DT_REG ){
+				
+				count++;
+				char path_name[strlen(folder_path) + 10];
+				strcpy(path_name, folder_path);
+				strcat(path_name, folder->d_name);
+			
+				FILE*arch = fopen(path_name, "rt");
+			
+				if(!arch)
+					return -1.00;
+			
+				float value;
+				while(fscanf(arch, "%f\n", &value) != EOF){
+					total += value;
+				}
+				fclose(arch);
+			}
+		}	
+		closedir(d);	
+	}
+	else
+		return 0;
+	return opc == 2? total:total/count;
+}
---
+++ ej3/ProcesoB.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#define err(msg, arg){ fprintf(stderr, "%s, vea %s -h\n", msg, arg); exit(1);}
+#define help(arg){ fprintf(stderr, "Ejemplo de ejecucion:\n ./path-del-ejecutable path-del-fifo\nEjemplo: %s pipe/fifo\n", arg); exit(1);}
+
+int pedir_anio(), pedir_mes();
+void main(int argc, char*argv[]){
+
+	if(argc > 1 && strcmp(argv[1], "-h") == 0){
+		help(argv[0]);
+	}
+
+	if(argc < 2)
+		err("Argumentos insuficientes", argv[0]);
+
+	char*meses[] = { "enero", "febrero", "marzo", 
+				 "abril", "mayo", "junio", 
+				 "julio", "agosto", "septiembre", 
+				 "octubre", "noviembre", "diciembre" };
+
+	printf("\tMenu:\n1- Facturacion Mensual.\n2- Facturacion Anual.\n3- Facturacion Media Anual.\n4- Salir.\n");		
+	
+	int opc, anio;
+	char*mes = (char*)malloc(sizeof(char)*strlen("septiembre"));
+	char* instruccion = (char*)malloc(sizeof(char)*50);
+	
+	do{	
+		scanf("%d", &opc);
+		
+		if(opc > 0 && opc < 4){		
+			sprintf(instruccion, "%d-%d/-%s.txt", opc, (anio = pedir_anio()), opc==1? mes = meses[pedir_mes()-1]:"");
+			break;
+		}
+
+		if(opc == 4){	
+			sprintf(instruccion, "%d-", opc);
+			return;
+		}
+		printf("Opcion Invalida. Ingrese Nuevamente..\n");
+
+	}while(1);
+
+	int fd = open(argv[1], O_WRONLY);
+	write(fd, instruccion, strlen(instruccion));
+	close(fd);
+		
+	float res;
+	fd = open(argv[1], O_RDONLY);
+	read(fd, &res, sizeof(res));
+	close(fd);
+
+	printf(res == -1.00 ?"No existe facturacion correspondiente": "Facturacion correspondiente");
+	printf(strlen(mes) != 0? " a %s del %d: ": " al %s%d:", mes,anio);
+	printf(res != -1.00?" %.2f\n":"\n", res);
+
+	
+    return;
+}
+
+int pedir_anio(){
+	int anio;
+	do{
+		printf("Ingrese año de facturacion: ");
+		scanf("%d", &anio);
+		if( anio < 0 || anio > 2021)
+			printf("Año invalido. Ingrese nuevamente..\n");
+		else	
+			break;
+	}while(1);
+	return anio;
+}
+int pedir_mes(){
+	int mes;
+	do{
+		printf("Ingrese mes de facturacion (Formato: MM): ");
+		scanf("%d", &mes);
+		if(mes < 1 || mes > 12)
+			printf("Mes invaldo. Ingrese nuevamente.. \n");
+		else
+			break;
+	}while(1);
+	return mes;
+}
